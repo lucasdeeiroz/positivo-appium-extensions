c65672e1e1a1c0b9d6bc74e793794f2352519b9a
Diogo Dorea <diogodorea@gmail.com>
2025-07-17 08:30:21 -0300

    refactor: Enhance documentation and improve keyword definitions. Added new auxiliary keyword to retrieve appPackage

--- keywords/TerminateApplication/TerminateApplicationExtension.py
+++ keywords/TerminateApplication/TerminateApplicationExtension.py
@@ -1,6 +1,11 @@
 from robot.api.deco import keyword
 from robot.libraries.BuiltIn import BuiltIn
 
+#  Library to handle application termination in Appium
+#  This library provides keywords to terminate an application
+#  Adictionaly, it allows retrieval of the current application ID(appPackage) and activity(appActivity).
+#  This is useful for testing scenarios where you need to ensure the application is closed
+
 class TerminateApplicationExtension:
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
@@ -15,3 +20,8 @@ class TerminateApplicationExtension:
     def terminate_application(self, app_id):
         """Termina o aplicativo especificado pelo app_id."""
         self.driver.terminate_app(app_id)
+
+    @keyword("Get Current App Id")
+    def get_current_app_id(self):
+        """Retorna o appPackage (app_id) da sessão atual."""
+        return self.driver.desired_capabilities.get("appPackage")
